3e9c8a45b59c4026c27c74cf35688f591167348a
Jackson Leb <jacksonleb@Jacksons-MacBook-Pro.local>
2022-03-19 15:17:13 -0400

    Began cleaning up context files

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -5,17 +5,25 @@ import {Link} from 'react-router-dom'
 import Spinner from '../components/layout/Spinner'
 import GithubContext from '../context/github/GithubContext'
 import {useParams} from 'react-router-dom'
+import {getUser, getUserRepos} from '../context/github/GithubActions'
 import RepoList from '../components/repos/RepoList'
 
 function User() {
 
-  const {getUser, getUserRepos, user, loading, repos} = useContext(GithubContext)
+  const {user, loading, repos, dispatch} = useContext(GithubContext)
 
   const params = useParams()
 
   useEffect(() => {
-    getUser(params.login)
-    getUserRepos(params.login)
+    dispatch({type: 'SET_LOADING'})
+    const getUserData = async() => {
+        const userData = await getUser(params.login)
+        dispatch({type: 'GET_USER', payload: userData})
+
+        const userRepoData = await getUserRepos(params.login)
+        dispatch({type: 'GET_REPOS', payload: userRepoData})
+    }
+    getUserData()
   }, [])
 
   const {
